4a81646650c57afa1bf846a76311f48674c3ae07
Kaleb Eyasu <kalu4mom@gmail.com>
2025-05-30 15:26:09 +0300

    Update service_provider.py

--- schemas/service_provider.py
+++ schemas/service_provider.py
@@ -5,7 +5,7 @@ class ServiceProviderCreate(BaseModel):
     name: str = Field(..., min_length=1, max_length=50)
     email: EmailStr
     first_name: str
-    Middle_name: str
+    middle_name: str
     last_name: str
     address: str = Field(..., min_length=1, max_length=100)
     nearby: str = Field(..., min_length=1, max_length=100)
@@ -20,7 +20,7 @@ class ServiceProviderResponse(BaseModel):
     name: str
     email: EmailStr
     first_name: str
-    Middle_name: str
+    middle_name: str
     last_name: str
     address: str
     nearby_condominum: str
