a84d5b86a38e231f6c6bb5195bcbb03fc742fa57
Kaleb Eyasu <kalu4mom@gmail.com>
2025-05-30 13:43:38 +0300

    Update service_provider.py

--- schemas/service_provider.py
+++ schemas/service_provider.py
@@ -2,18 +2,29 @@ from pydantic import BaseModel, Field , EmailStr
 class ServiceProviderCreate(BaseModel) :
     name: str = Field(..., min_length=1, max_length=50)
     email: EmailStr
+    first_name:str
+    Middle_name:str
+    last_name:str
     address: str = Field(..., min_length=1, max_length=100)
     nearby: str = Field(..., min_length=1, max_length=100)
     phone_number: int
+    date_of_birth:date
+    washing_machine:boolean 
     longitude: float
     latitude: float
 class ServiceProviderResponse(BaseModel) :
     id: int
     name: str
+    first_name:str
+    Middle_name:str
+    last_name:str
+    washing_machine:str
     email: EmailStr
     phone_number: int
     address: str
     nearby: str
+    date_of_birth:date
+    washing_machine:boolean
     longitude: float
     latitude: float
 
