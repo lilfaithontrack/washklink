8cefb00d12d34de85aae16f5db7d1c7a2b186c41
Kaleb Eyasu <kalu4mom@gmail.com>
2025-05-30 15:44:37 +0300

    Update service_provider.py

--- controllers/service_provider.py
+++ controllers/service_provider.py
@@ -18,7 +18,7 @@ def create_service_provider(db: Session, service_provider: ServiceProviderCreate
     db_service_provider = ServiceProvider(
         name=service_provider.name,
         first_name=service_provider.first_name,
-        middle_name=service_provider.Middle_name,
+        middle_name=service_provider.middle_name,
         last_name=service_provider.last_name,
         email=service_provider.email,
         address=service_provider.address,
