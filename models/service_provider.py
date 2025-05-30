6db684f1dd5bfb054aae261bf9926269af8b8da5
Kaleb Eyasu <kalu4mom@gmail.com>
2025-05-30 13:34:57 +0300

    Update service_provider.py

--- models/service_provider.py
+++ models/service_provider.py
@@ -4,15 +4,15 @@ from database import Base
 class ServiceProvider(Base) :
     __tablename__ = "service_provider"
     id = Column(Integer, unique=True, primary_key=True, index=True)
-    first_name=Column(String, nullable=False)
+    first_name= Column(String, nullable=False)
     Middle_name= Column(String, nullable=False)
     last_name= Column(String, nullable=False)
     name = Column(String,  nullable=False)
-    phone_number=Column(Integer, unique=True, nullable=False) 
+    phone_number= Column(Integer, unique=True, nullable=False) 
     email = Column(String,unique=True, index=True , nullable=False)
     password = Column(String, nullable=True)
     is_active = Column(Boolean, default=True)
-    Date_of_Birth=Column(Date, nullable=False)
+    Date_of_Birth= Column(Date, nullable=False)
     address= Column(String, nullable=False)
     washing_machine=(Boolean, default=True)
     nearby = Column(String, nullable=False)
