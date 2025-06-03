7db5ab559d547ac3ccf61fe76efe91bbe3d07518
Kaleb Eyasu <kalu4mom@gmail.com>
2025-06-03 15:47:54 +0300

    Update users.py

--- models/users.py
+++ models/users.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, String, Boolean
 from database import Base
 
 class DBUser(Base):
-    __tablename__ = "users"
+    __tablename__ = "new_users"
 
     id = Column(Integer, primary_key=True, index=True, unique=True)
     phone_number = Column(String, unique=True, nullable=False, index=True)
