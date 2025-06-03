d7ce2fcf2e891438d37441278b60a41d0f352525
Kaleb Eyasu <kalu4mom@gmail.com>
2025-06-03 15:47:21 +0300

    Create users.py

---
+++ models/users.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from database import Base
+
+class DBUser(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True, unique=True)
+    phone_number = Column(String, unique=True, nullable=False, index=True)
+    is_active = Column(Boolean, default=True)
