b33eba3f84c398b81f43fe6aaa6c305a812d993e
Kaleb Eyasu <kalu4mom@gmail.com>
2025-06-04 23:25:46 +0300

    Update booking.py

--- schemas/booking.py
+++ schemas/booking.py
@@ -1,20 +1,30 @@
 from pydantic import BaseModel
 from typing import Optional
 
+
 class BookingCreate(BaseModel):
+    user_id: int
+    product_id: int
+    category_id: int
+    payment_option: Optional[str]
+    delivery: bool = False
+    delivery_km: float = 0.0
+    cash_on_delivery: bool = False
+    note: Optional[str] = None
+
+
+class BookingOut(BaseModel):
+    id: int
     user_id: int
     item: str
     price_tag: float
     subtotal: float
     payment_option: Optional[str]
-    delivery: bool = False
-    delivery_km: float = 0.0
-    delivery_charge: float = 0.0
-    cash_on_delivery: bool = False
-    note: Optional[str] = None
-
-class BookingOut(BookingCreate):
-    id: int
+    delivery: bool
+    delivery_km: float
+    delivery_charge: float
+    cash_on_delivery: bool
+    note: Optional[str]
 
     class Config:
         orm_mode = True
