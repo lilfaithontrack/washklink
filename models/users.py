7cc3918853821a2174fe6f4d6224a3e2a2698c40
Kaleb Eyasu <kalu4mom@gmail.com>
2025-06-05 16:02:23 +0300

    Update users.py

--- models/users.py
+++ models/users.py
@@ -4,8 +4,10 @@ from database import Base
 
 class DBUser(Base):
     __tablename__ = "new_users"
-
-    id = Column(Integer, primary_key=True, index=True, unique=True)
+    
+    id = Column(Integer, primary_key=True, index=True, unique=True) 
+    sender_name= Column(String(255) , nullable= False)
+    phone_number= Column(String(255) , nullable= False)
     is_active = Column(Boolean, default=True)
 
     # One-to-many: one user can have many bookings
