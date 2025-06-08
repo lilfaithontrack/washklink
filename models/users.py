67b1a0c79c8b7118150ddc3ebf84523cb345bb44
Kaleb Eyasu <kalu4mom@gmail.com>
2025-06-08 12:04:30 +0300

    Update users.py

--- models/users.py
+++ models/users.py
@@ -8,7 +8,7 @@ class DBUser(Base):
     id = Column(Integer, primary_key=True, index=True, unique=True)
     full_name = Column(String(255), nullable=False)  # full name from Google
     email = Column(String(255), unique=True, nullable=False)  # email from Google Auth
-    phone_number = Column(String(255), nullable=False)
+    phone_number = Column(String(255), nullable=True)
     is_active = Column(Boolean, default=True)
 
     # One-to-many: one user can have many bookings
